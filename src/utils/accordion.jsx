5f364957955c830074abfd064645fd5b8182d314
MogammadShaqeelless16 <shaqeelless4@gmail.com>
2024-04-20 22:57:31 +0200

    first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Mission",
+    detail:
+      "To simplify and enhance the childcare journey for families by connecting them with quality creche options, ensuring peace of mind and support for every child's development.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Value",
+    detail:
+      "We prioritize transparency, safety, and excellence in childcare, fostering a trusted community for families and creches alike.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Goal",
+    detail:
+      "Our aim is to become the go-to platform for families seeking reliable childcare solutions and to continuously improve the standards of care across all creche facilities.",
+  },
+];
+export default data;
