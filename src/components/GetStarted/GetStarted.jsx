3187b6ccddd5ca0cc2eb9a6968f79514f7277fe6
Thuthukani Langa <104025247+Thuthukanii@users.noreply.github.com>
2024-07-27 08:50:44 +0000

    Update

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -12,7 +12,7 @@ const GetStarted = () => {
             Find your creche soon
           </span>
           <button className="button" href>
-            <a href="mailto:zainkeepscode@gmail.com">Get Started</a>
+            <a href="mailto:Crechespots@gmail.com">Get Started</a>
           </button>
         </div>
       </div>
