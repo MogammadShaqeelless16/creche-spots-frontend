a42c2d92ed2c9fc80c75d00fc8965b6aea7a426d
Thuthukani Langa <104025247+Thuthukanii@users.noreply.github.com>
2024-07-28 07:54:38 +0000

    Update

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -5,7 +5,7 @@ const Companies = () => {
     <section className="c-wrapper">
       <div className="paddings innerWidth flexCenter c-container">
         <img src="./prologis.png" alt="" />
-        <img src="./tower.png" alt="" />
+        <img src="./Waltons.png" alt="" />
         <img src="./AYOBA.jpg" alt="" />
         <img src="./Chenosis.png" alt="" />
       </div>
